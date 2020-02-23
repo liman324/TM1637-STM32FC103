0cf7db1cf9ff3e07b93cd82f431be9cd14eb1697
Alexander Liman <liman324@yandex.ru>
2020-02-23 23:31:37 +0600

    Update STM32_TM1637.cpp

--- STM32_TM1637.cpp
+++ STM32_TM1637.cpp
@@ -1,59 +1,70 @@
 #include <STM32_TM1637.h>
 
-STM32_TM1637::STM32_TM1637(){};
+STM32_TM1637::STM32_TM1637(uint8_t clk, uint8_t dio){
+         CLK = clk;
+         DIO = dio;
+};
 
 void STM32_TM1637::brig(byte br){
      qwer = br + CODE0;
   }
 
+void STM32_TM1637::del(){delayMicroseconds(100);}
+
+
 void STM32_TM1637::dec(byte dig){
          for(int i = 0; i < 8; i++) {
-           pinMode(CLK, OUTPUT);
-           delayMicroseconds(100);
+           pinMode(CLK, OUTPUT);del();
          if (dig & 0x01)
            pinMode(DIO, INPUT);
          else
-           pinMode(DIO, OUTPUT);
-           delayMicroseconds(100);
-           pinMode(CLK, INPUT);
-           delayMicroseconds(100);
+           pinMode(DIO, OUTPUT);del();
+           pinMode(CLK, INPUT);del();
            dig = dig >> 1;
   }
            pinMode(CLK, OUTPUT);
-           pinMode(DIO, INPUT);
-           delayMicroseconds(100);
-           pinMode(CLK, INPUT);
-           delayMicroseconds(100);
+           pinMode(DIO, INPUT);del();
+           pinMode(CLK, INPUT);del();
            uint8_t ack = digitalRead(DIO);
          if (ack == 0)
-           pinMode(DIO, OUTPUT);
-           delayMicroseconds(100);
-           pinMode(CLK, OUTPUT);
-           delayMicroseconds(100);
+           pinMode(DIO, OUTPUT);del();
+           pinMode(CLK, OUTPUT);del();
   }
             
 void STM32_TM1637::stop(){
-           pinMode(DIO, OUTPUT);
-           delayMicroseconds(100);
-           pinMode(CLK, INPUT);
-           delayMicroseconds(100);
-           pinMode(DIO, INPUT);
-           delayMicroseconds(100);
+           pinMode(DIO, OUTPUT);del();
+           pinMode(CLK, INPUT);del();
+           pinMode(DIO, INPUT);del();
   }  
 
 void STM32_TM1637::start(){
-           pinMode(DIO, OUTPUT);
-           delayMicroseconds(100);
+           pinMode(DIO, OUTPUT);del();
   }
 
-void STM32_TM1637::print_dec(int decimal, int pd,    int h0, int h1, int h2, int h3){
+void STM32_TM1637::print_float(float decimal, byte pd,    byte h0, byte h1, byte h2, byte h3){
         start();dec(qwer);stop();start();dec(CODE1);stop();start();
-      
-        if(decimal<0){decimal = abs(decimal);if(pd>9){pd = pd - 10;}}
-        int data0 = decimal/1000;
-        int data1 = decimal/100%10;
-        int data2 = decimal/10%10;
-        int data3 = decimal%10;
+
+        byte minus;
+        int mn;
+        
+        switch(pd){
+          case 0: mn = 1;break;
+          case 1: mn = 10;break;
+          case 2: mn = 100;break;
+          case 3: mn = 1000;break;
+          }
+        
+        if(decimal<0){decimal = abs(decimal); minus = 1;}else{minus = 0;}
+        
+        //if(decimal/1000>=1){pd = 0;}
+        if(decimal/100>=1&&decimal/100<10){decimal = round(decimal*mn);}
+        if(decimal/10>=1&&decimal/10<10){decimal = round(decimal*mn);}
+        if(decimal>=0&&decimal<10){decimal = round(decimal*mn);}
+
+        int data0 = (int)decimal/1000;
+        int data1 = (int)decimal/100%10;
+        int data2 = (int)decimal/10%10;
+        int data3 = (int)decimal%10;
         
         for(int n=0;n<4;n++){
         int data;
@@ -81,23 +92,39 @@ void STM32_TM1637::print_dec(int decimal, int pd,    int h0, int h1, int h2, int
         if(n==3){data3=data;}
         }
         
-        byte w;
-        if(pd>9){pd = pd -10;w=0b00111111;}else{w=0b00000000;}
-        if(decimal<1000&&decimal>99){data0 = w;}
-        if(decimal<100&&decimal>9){data0 = w;data1 = w;}
-        if(decimal<10){data0 = w; data1 = w; data2 = w;}
-        
+        if(minus==0){
+        if(pd==3&&decimal<1000){data0 = 0b00111111;}
+        if(pd==2&&decimal<100){data0 = 0b00000000;data1 = 0b00111111;}
+        if(pd==2&&decimal<1000){data0 = 0b00000000;}
+        if(pd==0&&decimal<10){data0 = 0b00000000;data1 = 0b00000000;data2 = 0b00000000;}
+        if(pd==0&&decimal<100){data0 = 0b00000000;data1 = 0b00000000;}
+        if(pd==0&&decimal<1000){data0 = 0b00000000;}
+        if(pd==1&&decimal<1000){data0 = 0b00000000;}
+        if(pd==1&&decimal<100){data0 = 0b00000000;data1 = 0b00000000;}
+        if(pd==1&&decimal<10){data0 = 0b00000000;data1 = 0b00000000;data2 = 0b00111111;}
+        if(decimal>9999){data0 = 0b01000000;data1 = 0b01000000;data2 = 0b01000000;data3 = 0b01000000;}
+        }
+
+        if(minus==1){
+        if(pd==0&&decimal<10){data0 = 0b00000000;data1 = 0b00000000;data2 = 0b01000000;}
+        if(pd==0&&decimal<100&&decimal>9){data0 = 0b00000000;data1 = 0b01000000;}
+        if(pd==0&&decimal<1000&&decimal>99){data0 = 0b01000000;}
+        if(pd==1&&decimal<100){data0 = 0b00000000;data1 = 0b01000000;}
+        if(pd==1&&decimal<1000&&decimal>=100){data0 = 0b01000000;}
+        if(pd==2&&decimal<1000){data0 = 0b01000000;}
+        if(decimal>1000){data0 = 0b01000000;data1 = 0b01000000;data2 = 0b01000000;data3 = 0b01000000;}
+        }
+ 
         switch(pd){
-        case 0 : data0;data1;data2;data3;break;
         case 1 : data2 = data2+0b10000000;break;
         case 2 : data1 = data1+0b10000000;break;
         case 3 : data0 = data0+0b10000000;break;
         }
-        
-        if(h0>-1){data0 = h0;}
-        if(h1>-1){data1 = h1;}
-        if(h2>-1){data2 = h2;}
-        if(h3>-1){data3 = h3;}
+
+        if(h0>0){data0 = h0;}
+        if(h1>0){data1 = h1;}
+        if(h2>0){data2 = h2;}
+        if(h3>0){data3 = h3;}
         
         dec(ADDR0);dec(data0);dec(data1);dec(data2);dec(data3);stop();
     }
